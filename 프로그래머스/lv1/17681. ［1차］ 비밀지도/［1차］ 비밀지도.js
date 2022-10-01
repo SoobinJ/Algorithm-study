9eb430689dea8a11de1280371687fefd05f7a292
전수빈 <soobin0818@konkuk.ac.kr>
2022-10-01 22:54:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -6,11 +6,11 @@ function solution(n, arr1, arr2) {
         var b1=(arr1[i].toString(2)).padStart(n,0);
         var b2=(arr2[i].toString(2)).padStart(n,0);
 
-        var k=b1.split("");
-        var t=b2.split("");
+        var sB1=b1.split("");
+        var sB2=b2.split("");
         
         for(var j=0;j<n;j++){
-            if(k[j]==='0'&&t[j]==='0'){
+            if(sB1[j]==='0'&&sB2[j]==='0'){
                 str=str+" ";
             }else{
                 str=str+"#";
