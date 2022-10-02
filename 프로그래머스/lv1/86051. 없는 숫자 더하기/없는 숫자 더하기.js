c187d445390bc43f8a8c0599ff365a9f6e854b48
전수빈 <soobin0818@konkuk.ac.kr>
2022-10-03 00:57:07 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    var answer = 0;
+    for(var i=0;i<10;i++){
+        if(numbers.find(v=>v===i)===undefined){
+            answer=answer+i;
+            console.log(i);
+        }
+    }
+    return answer;
+}
