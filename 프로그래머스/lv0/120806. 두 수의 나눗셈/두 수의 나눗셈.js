e479800a7d670d7fff8dc3601ccb1b9ab8625bef
전수빈 <soobin08011812@gmail.com>
2023-10-18 11:36:15 +0900

    lv0 - 두 수의 나눗셈 다른 사람 풀이 및 설명 추가

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,3 +1,11 @@
 function solution(num1, num2) {
-    return parseInt(num1/num2*1000)
+  return parseInt((num1 / num2) * 1000);
+}
+
+//다른 사람 풀이
+// Math.trunc() - 소수점 이하는 버림, Math.floor() - 내림
+// => Math.trunc()의 경우 소수점 이하는 다 버리기 때문에 -23.2인 경우 -23을 return 함
+// 하지만 Math.floor()의 경우 -23.3인 경우 내림을 하기 때문에 -24를 return 함
+function solution(num1, num2) {
+  return Math.trunc((num1 / num2) * 1000);
 }
