1170e89b212c3605169dc243eca0252437600033
전수빈 <soobin08011812@gmail.com>
2023-10-18 11:32:28 +0900

    lv0 - 분수의 덧셈 주석 추가

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,6 +1,8 @@
 const getGCD = (num1, num2) => {
   let gcd = 1;
 
+  // 최대공약수는 num1, num2의 공통된 약수 중 가장 큰 정수
+  // 2부터 num1,num2 중 작은 값까지 모든 정수로 나눔, 둘다 나누어 떨어지는 수를 재할당하며 반복
   for (let i = 2; i <= Math.min(num1, num2); i++) {
     if (num1 % i === 0 && num2 % i === 0) {
       gcd = i;
