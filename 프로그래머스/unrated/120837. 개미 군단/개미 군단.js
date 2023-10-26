f0b1e85d169af7e46ba75a84d1241cb289ef4833
전수빈 <soobin08011812@gmail.com>
2023-10-26 20:36:19 +0900

    [level unrated] Title: 개미 군단, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,7 @@
 function solution(hp) {
     const hpArr = [5,3,1]
     let sum =0
-    const a = hpArr.reduce((acc,el,i)=>{
+    hpArr.reduce((acc,el,i)=>{
         const attack = i===2?Math.ceil(acc/el):Math.floor(acc/el)
         sum = sum+attack
         return acc-attack*el
