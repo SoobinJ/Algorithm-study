d5d75df0bc980cc8855a7f4fdc538ba8cb967234
전수빈 <soobin08011812@gmail.com>
2023-11-03 12:34:02 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,23 @@
+    const arr=
+        {
+           'zero':0,
+            'one':1,
+            'two':2,
+            'three':3,
+            'four':4,
+            'five':5,
+            'six':6,
+            'seven':7,
+            'eight':8,
+            'nine':9
+        }
+
+function solution(numbers) {
+    const newArr = Object.keys(arr).reduce((acc,el)=>{
+        return acc.split(el).join(` ${el} `)
+    },numbers).trim().split('  ')
+    
+    return Number(newArr.reduce((acc,el)=>{
+        return acc=acc+arr[Object.keys(arr).find(key=>arr[key]===arr[el])]
+    },''))
+}
