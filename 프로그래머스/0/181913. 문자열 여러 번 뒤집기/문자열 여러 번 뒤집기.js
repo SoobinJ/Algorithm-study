38c56eed1aaa92de5e387a16bcde97632bb84b1b
전수빈 <soobin08011812@gmail.com>
2024-03-14 20:03:40 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 8.24 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, queries) {
+    return queries.reduce((acc,el,i)=>{
+        const strReverse = acc.slice(el[0],el[1]+1).split('').reverse().join('')
+        return acc.slice(0,el[0])+strReverse+acc.slice(el[1]+1)
+    },my_string)
+}
