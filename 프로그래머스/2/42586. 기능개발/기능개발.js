ccbc6076951414f911fe7a96add92acd5e4f5392
전수빈 <soobin08011812@gmail.com>
2024-04-08 23:28:50 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,25 @@
 function solution(progresses, speeds) {
-    var answer = [];
-  
-    const arr = progresses.map((e,index)=>Math.ceil((100-e)/speeds[index]))
-    
-    let count = 1
-    let num = arr[0]
-    let idx = 1
-    
-    while(idx<arr.length) {
-        if(num>=arr[idx]) {
-            count++
-        }else{
-            answer.push(count)
-            num = arr[idx]
-            count=1
-        }
-        idx++
+  var answer = [];
+  let count = 1;
+
+  const arr = progresses.map((e, index) =>
+    Math.ceil((100 - e) / speeds[index])
+  );
+
+  let num = arr[0];
+  let idx = 1;
+
+  while (idx < arr.length) {
+    if (num >= arr[idx]) {
+      count++;
+    } else {
+      answer.push(count);
+      num = arr[idx];
+      count = 1;
     }
-    answer.push(count)
-    
-    return answer;
+    idx++;
+  }
+  answer.push(count);
+
+  return answer;
 }
