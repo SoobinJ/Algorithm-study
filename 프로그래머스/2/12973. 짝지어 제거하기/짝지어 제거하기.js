8580775f5fcead545228d4a84faf396a4c9a4310
전수빈 <soobin08011812@gmail.com>
2024-10-01 23:40:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 100.21 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,15 +1,11 @@
-function solution(s)
-{
-    let arr=[]
-
+function solution(s){
+    let stack=[]
     for(let i=0;i<s.length;i++) {
-        if(s[i]===arr.at(-1)) {
-            arr.pop()
-        }else{
-            arr.push(s[i])
+        if(stack.length>0&&stack.at(-1)===s[i]){
+            stack.pop()
+        }else {
+            stack.push(s[i])      
         }
     }
-    
-    return arr.length===0?1:0
-    
+    return stack.length===0?1:0
 }
